e87a1ebba952072ff5586ca094fd2e2114d84b75
Dszymczyk <damian.szymczyk1@gmail.com>
2019-12-11 17:35:53 +0100

    Add interface IBasket and clas Basket(There are compilation errors)

---
+++ InterfacesTest/Basket.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesTest
+{
+    class Basket
+    {
+        public struct BasketPosition
+        {
+            public Bread product;
+            public int amount;
+
+            public BasketPosition(Bread prod, int amt)
+            {
+                product = prod;
+                amount = amt;
+            }
+            public BasketPosition(Bread prod)
+            {
+                product = prod;
+                amount = 1;
+            }
+        }
+        List<BasketPosition> BasketList = new List<BasketPosition>();
+
+        public void AddToBasket(params BasketPosition[] PositionToAdd)
+        {
+            foreach (BasketPosition element in BasketList)
+            {
+                if(element.product == PositionToAdd.product)
+                {
+
+                }
+            }
+        }
+        // public void addToBasket(params Bread[] product); product + amount
+        public void RemoveFromBasket(BasketPosition positionToRemove)
+        {
+
+        }
+        public void BuyFromBasket()
+        {
+
+        }
+    }
+}
---
+++ InterfacesTest/IBasket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesTest
+{
+    interface IBasket
+    {
+        public void AddToBasket(params Bread[] product);
+        // public void addToBasket(params Bread[] product); product + amount
+        public void RemoveFromBasket(Bread product);
+        public void BuyFromBasket();
+    }
+}
